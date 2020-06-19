c6ee1fe8922f1c9f3182b7872e093c6125e465d0
Grazina Gimbutiene <grazina.laurinaviciute@gmail.com>
2020-06-19 09:33:26 +0300

    Add Swagger to produce API examples.

--- src/main/java/lt/gimbutiene/presidentElection/controller/ElectionController.java
+++ src/main/java/lt/gimbutiene/presidentElection/controller/ElectionController.java
@@ -1,5 +1,6 @@
 package lt.gimbutiene.presidentElection.controller;
 
+import io.swagger.annotations.ApiOperation;
 import lt.gimbutiene.presidentElection.dto.*;
 import lt.gimbutiene.presidentElection.exception.VoterServiceException;
 import lt.gimbutiene.presidentElection.facade.CandidateFacade;
@@ -20,26 +21,33 @@ public class ElectionController {
     private VoterFacade voterFacade;
 
     @GetMapping("/getCandidates")
+    @ApiOperation(value = "Get the list of candidates")
     public List<CandidateDto> getCandidates() {
         return candidateFacade.getCandidates();
     }
 
+
     @PostMapping("/vote")
+    @ApiOperation(value = "Vote for a candidate")
     public void vote(@RequestBody final VoteDto voteDto) throws VoterServiceException {
         voterFacade.addVote(voteDto);
     }
 
     @GetMapping("/getElectionResults")
+    @ApiOperation(value = "Get election result for each candidate")
     public ElectionResultsByCandidateDto getElectionResults() {
         return voterFacade.getElectionResultsByCandidate();
     }
 
+
     @GetMapping("/getElectionResultsByRegion")
+    @ApiOperation(value = "Get election result for each candidate by region")
     public List<RegionResultsDto> getElectionResultsByRegion() {
         return voterFacade.getElectionResultsByRegion();
     }
 
     @GetMapping("/getElectionWinner")
+    @ApiOperation(value = "Get the winner of the election")
     public WinnerDto getElectionWinner() {
         return voterFacade.getElectionWinner();
     }
