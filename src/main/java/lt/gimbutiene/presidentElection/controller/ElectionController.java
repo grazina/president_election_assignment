254eee009b1d60337ae32ba5de05b08b3f94618f
Grazina Gimbutiene <grazina.laurinaviciute@gmail.com>
2020-06-19 11:30:38 +0300

    Update api path.

--- src/main/java/lt/gimbutiene/presidentElection/controller/ElectionController.java
+++ src/main/java/lt/gimbutiene/presidentElection/controller/ElectionController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("election")
+@RequestMapping("/api/election")
 public class ElectionController {
 
     @Autowired
